4d08399942d0405aebb44a2cca6e8b56bb9bd47e
Vicente Jesus Lechuga <vicho0202@live.com>
2020-06-02 03:24:40 -0400

    Agregar archivos de proyecto.

---
+++ Folder2OTD/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace Folder2OTD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.FileMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.selectFolder = new System.Windows.Forms.ToolStripMenuItem();
+            this.ltbFolders = new System.Windows.Forms.ListBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.Infobox = new System.Windows.Forms.RichTextBox();
+            this.cbTrasp = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbFormat = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkRecursive = new System.Windows.Forms.CheckBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.menuStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.FileMenu});
+            resources.ApplyResources(this.menuStrip1, "menuStrip1");
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
+            // 
+            // FileMenu
+            // 
+            this.FileMenu.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.selectFolder});
+            this.FileMenu.Name = "FileMenu";
+            resources.ApplyResources(this.FileMenu, "FileMenu");
+            // 
+            // selectFolder
+            // 
+            this.selectFolder.Name = "selectFolder";
+            resources.ApplyResources(this.selectFolder, "selectFolder");
+            this.selectFolder.Click += new System.EventHandler(this.selectFolder_Click);
+            // 
+            // ltbFolders
+            // 
+            this.ltbFolders.FormattingEnabled = true;
+            resources.ApplyResources(this.ltbFolders, "ltbFolders");
+            this.ltbFolders.Name = "ltbFolders";
+            // 
+            // btnClear
+            // 
+            resources.ApplyResources(this.btnClear, "btnClear");
+            this.btnClear.Name = "btnClear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnConvert
+            // 
+            resources.ApplyResources(this.btnConvert, "btnConvert");
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // Infobox
+            // 
+            this.Infobox.BackColor = System.Drawing.Color.DarkGray;
+            this.Infobox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.Infobox.ForeColor = System.Drawing.Color.DarkGreen;
+            resources.ApplyResources(this.Infobox, "Infobox");
+            this.Infobox.Name = "Infobox";
+            this.Infobox.ReadOnly = true;
+            this.Infobox.TextChanged += new System.EventHandler(this.Infobox_TextChanged);
+            // 
+            // cbTrasp
+            // 
+            this.cbTrasp.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbTrasp.FormattingEnabled = true;
+            resources.ApplyResources(this.cbTrasp, "cbTrasp");
+            this.cbTrasp.Items.AddRange(new object[] {
+            resources.GetString("cbTrasp.Items"),
+            resources.GetString("cbTrasp.Items1"),
+            resources.GetString("cbTrasp.Items2")});
+            this.cbTrasp.Name = "cbTrasp";
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // cbFormat
+            // 
+            this.cbFormat.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbFormat.FormattingEnabled = true;
+            this.cbFormat.Items.AddRange(new object[] {
+            resources.GetString("cbFormat.Items")});
+            resources.ApplyResources(this.cbFormat, "cbFormat");
+            this.cbFormat.Name = "cbFormat";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // checkRecursive
+            // 
+            resources.ApplyResources(this.checkRecursive, "checkRecursive");
+            this.checkRecursive.Name = "checkRecursive";
+            this.checkRecursive.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnConvert);
+            this.groupBox1.Controls.Add(this.btnClear);
+            this.groupBox1.Controls.Add(this.checkRecursive);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.cbTrasp);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.cbFormat);
+            resources.ApplyResources(this.groupBox1, "groupBox1");
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.TabStop = false;
+            // 
+            // richTextBox1
+            // 
+            resources.ApplyResources(this.richTextBox1, "richTextBox1");
+            this.richTextBox1.BackColor = System.Drawing.Color.LightGray;
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Name = "richTextBox1";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.groupBox2.Controls.Add(this.ltbFolders);
+            this.groupBox2.Controls.Add(this.richTextBox1);
+            this.groupBox2.Controls.Add(this.groupBox1);
+            this.groupBox2.Controls.Add(this.Infobox);
+            resources.ApplyResources(this.groupBox2, "groupBox2");
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.HelpButton = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem FileMenu;
+        private System.Windows.Forms.ToolStripMenuItem selectFolder;
+        private System.Windows.Forms.ListBox ltbFolders;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.RichTextBox Infobox;
+        public System.Windows.Forms.ComboBox cbTrasp;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbFormat;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkRecursive;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
+
---
+++ Folder2OTD/Form1.cs
@@ -0,0 +1,336 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WK.Libraries.BetterFolderBrowserNS;
+
+namespace Folder2OTD
+{
+    public partial class Form1 : Form
+    {
+        public static int des;
+        public static int des2;
+        public static double operations;
+        public static double operations2;
+        public static bool IsTransp;
+        public static string command;
+        public static string folderblank;
+        public static string formatdds;
+        public static string[] directorio;
+        public static bool isEmpty;
+        public static string log;
+        public static BetterFolderBrowser bfb = new BetterFolderBrowser();
+        public static IEnumerable<String> imgfolders;
+
+        public Form1()
+        {
+            InitializeComponent();
+            isEmpty = true;
+            cbTrasp.SelectedIndex = 2;
+            cbFormat.SelectedIndex = 0;
+            CheckForIllegalCrossThreadCalls = false;
+            
+            
+        }
+
+        private void selectFolder_Click(object sender, EventArgs e)
+        {
+            bfb.Multiselect = true;
+            
+            bfb.ShowDialog();
+            ltbFolders.DataSource = bfb.SelectedFolders;
+            directorio = bfb.SelectedFolders;
+
+            if (bfb.SelectedFolder.Length >= 1)
+            {
+                isEmpty = false;
+            }
+            else
+            {
+                isEmpty = true;
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            Clear();
+
+        }
+
+        public bool IsTransparent(Bitmap image)
+        {
+            if (cbTrasp.SelectedIndex == 0)
+            {
+                //if (image.ToString().Contains(".tga"))
+                //{
+
+                //}
+                if ((image.Flags & 0x2) != 0)
+                {
+                    return IsTransp = true;
+                }
+                else
+                {
+                    return IsTransp = false;
+                }
+            }
+            if (cbTrasp.SelectedIndex == 1)
+            {
+                for (int y = 0; y < image.Height; ++y)
+                {
+                    for (int x = 0; x < image.Width; ++x)
+                    {
+                        if (image.GetPixel(x, y).A < 255)
+                        {
+                            return IsTransp = true;
+                        }
+                    }
+                }
+                return IsTransp = false;
+            }
+
+            if (cbTrasp.SelectedIndex == 2)
+            {
+                return IsTransp = false;
+            }
+            
+            
+            return IsTransp;
+
+        }
+
+        public string ReplaceName(string pngfile, bool Do)
+        {
+
+            string pngfole = Path.GetFileName(pngfile);
+            string cur = Path.GetDirectoryName(pngfile);
+
+            if (Do == true)
+            {
+                if (pngfole.Contains(" "))
+                {
+                    pngfole = pngfole.Replace(" ", "_");
+                    return cur + "\\" + pngfole;
+                }
+                return cur + "\\" + pngfole;
+            }
+            else
+            {
+                return cur + "\\" + pngfole;
+            }
+
+
+
+        }
+
+        public int CheckResolution(Bitmap image)
+        {
+
+            int mip = 0;
+            if (image.Width < image.Height)
+            {
+
+                for (double i = 1; i < 32; i++)
+                {
+                    operations = image.Width / Math.Pow(2, i);
+
+                    if (operations <= 2)
+                    {
+                        mip = Convert.ToInt32(i);
+                        break;
+                    }
+
+                }
+
+
+            }
+            else
+            {
+                for (double c = 1; c < 32; c++)
+                {
+                    operations = image.Height / Math.Pow(2, c);
+
+                    if (operations <= 2)
+                    {
+                        mip = Convert.ToInt32(c);
+                        break;
+                    }
+
+                }
+            }
+
+            return mip;
+
+        }
+
+        public async Task OpenIVOTD(string[] documento)
+        {
+            string DXTformat = "";
+            int mipmapotd = 0;
+            int indexcount = 0;
+            await Task.Run(() =>
+             {
+                 foreach (var carpeta in documento)
+                 {
+                     
+                     StringBuilder otdbuilder = new StringBuilder();
+                     otdbuilder.AppendLine("Version 13 30");
+                     otdbuilder.AppendLine("{");
+                     Infobox.AppendText("\nReading folder " + Path.GetFileName(carpeta) + "...");
+
+
+                     if (checkRecursive.Checked)
+                     {
+                         imgfolders = Directory.EnumerateFiles(carpeta, "*.*", SearchOption.AllDirectories).Where(s => s.EndsWith(".png") || s.EndsWith(".bmp") || s.EndsWith(".jpg") || s.EndsWith(".dds"));
+                     }
+                     else
+                     {
+                         imgfolders = Directory.EnumerateFiles(carpeta, "*.*", SearchOption.TopDirectoryOnly).Where(s => s.EndsWith(".png") || s.EndsWith(".bmp") || s.EndsWith(".jpg") || s.EndsWith(".dds"));
+                     }
+
+                     int imgcount = imgfolders.Count();
+
+                     if (imgcount == 0)
+                     {
+                         Infobox.AppendText("\n0 compatible textures, skipping...");
+
+                     }
+                     else
+                     {
+                         Infobox.AppendText("\n" + imgcount + " compatible textures.");
+
+                     }
+
+                     ltbFolders.SelectedIndex = indexcount++;
+                     foreach (var imgfiles in imgfolders)
+                     {
+
+                         if (imgfolders?.Any() != true)
+                         {
+
+
+                         }
+                         else
+                         {
+                             string filename = Path.GetFileName(imgfiles);
+                             string filename2 = Path.GetFileNameWithoutExtension(imgfiles);
+                             
+
+                             Infobox.AppendText("\nProcessing " + filename + "...");
+
+                             string foldername = Path.GetDirectoryName(imgfiles);
+
+                             if (imgfiles.Contains(".dds"))
+                             {
+                                 DXTformat = "DXT1";
+                                 mipmapotd = 1;
+                             }
+                             else
+                             {
+                                 Bitmap bp = new Bitmap(imgfiles);
+
+                                 if (IsTransparent(bp))
+                                 {
+                                     DXTformat = "DXT5";
+                                 }
+                                 else
+                                 {
+                                     DXTformat = "DXT1";
+
+                                 }
+
+                                 mipmapotd = CheckResolution(bp);
+
+
+
+                             }
+
+
+
+                             StringBuilder otxbuild = new StringBuilder();
+                             otxbuild.AppendLine("Version 13 30");
+                             otxbuild.AppendLine("{");
+                             otxbuild.AppendLine("	Image " + filename);
+                             otxbuild.AppendLine("	Type Regular");
+                             otxbuild.AppendLine("    PixelFormat " + DXTformat);
+                             otxbuild.AppendLine("	Levels " + mipmapotd);
+                             otxbuild.AppendLine("	Usage UNKNOWN");
+                             otxbuild.AppendLine("	UsageFlags -");
+                             otxbuild.AppendLine("	ExtraFlags 0");
+                             otxbuild.AppendLine("}");
+
+                             File.WriteAllText(carpeta + "//" + filename2 + ".otx", otxbuild.ToString());
+
+                             otdbuilder.AppendLine("	" + Path.GetFileName(foldername).Replace("/", "") + "\\" + filename2 + ".otx");
+                         }
+
+
+
+                     }
+
+                     otdbuilder.AppendLine("}");
+                     File.WriteAllText(carpeta + ".otd", otdbuilder.ToString());
+                 }
+             }
+
+
+                ).ConfigureAwait(false);
+
+        }
+
+        private async void btnConvert_Click(object sender, EventArgs e)
+        {
+
+            Infobox.Text = string.Empty;
+
+            if (isEmpty)
+            {
+                MessageBox.Show("No folders selected.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                DisableControls();
+                await OpenIVOTD(bfb.SelectedFolders).ConfigureAwait(false);
+                MessageBox.Show("Done", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                EnableControls();
+            }
+        }
+
+
+        private void Infobox_TextChanged(object sender, EventArgs e)
+        {
+            Infobox.SelectionStart = Infobox.Text.Length;
+            Infobox.ScrollToCaret();
+        }
+
+        private void Clear()
+        {
+            ltbFolders.DataSource = null;
+            isEmpty = true;
+            Infobox.Text = string.Empty;
+        }
+
+        private void DisableControls()
+        {
+            btnClear.Enabled = false;
+            btnConvert.Enabled = false;
+            cbFormat.Enabled = false;
+            cbTrasp.Enabled = false;
+            checkRecursive.Enabled = false;
+        }
+
+        private void EnableControls()
+        {
+            btnClear.Enabled = true;
+            btnConvert.Enabled = true;
+            cbFormat.Enabled = true;
+            cbTrasp.Enabled = true;
+            checkRecursive.Enabled = true;
+        }
+    }
+}
